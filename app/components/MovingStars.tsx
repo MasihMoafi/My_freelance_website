03d62f12cf47d37925ea510aca7343f92954cb0f
MasihMoafi <masihmoafi12@gmail.com>
2025-08-18 20:20:15 +0330

    🔧 Slow down stars more - speed now 0.05-0.15

--- app/components/MovingStars.tsx
+++ app/components/MovingStars.tsx
@@ -40,7 +40,7 @@ const MovingStars: React.FC<MovingStarsProps> = ({ starColor = '#ffffff' }) => {
         originalX: x,
         originalY: y,
         size: Math.random() * 2 + 0.5,
-        speed: Math.random() * 0.5 + 0.3,
+        speed: Math.random() * 0.1 + 0.05,
         opacity: Math.random() * 0.8 + 0.2
       });
     }
